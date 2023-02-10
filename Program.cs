58cb1fd952403e8fd93d544134e3824900ba377f
aliffianilhamf <111202214155@mhs.dinus.ac.id>
2023-02-10 19:30:36 +0700

    add a program  utility class

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ class Program
     }
 }
 */
-
+/*
 //CONSTRUCTOR CHAINING
 class Mobil
 {
@@ -96,3 +96,73 @@ class Program
 
     }
 }
+*/
+/*
+//UTILITY CLASS
+class KonversiSuhu
+{
+    public static double celciusKeFahrenheit(string suhuCelcius)
+    {
+        //konversi(parse) ke bentuk double
+        double celcius = double.Parse(suhuCelcius);
+
+        //masukkan rumus
+        double fahrenheit = (celcius * 9 / 5) + 32;
+
+        //return
+        return fahrenheit;
+
+    }
+
+    public static double fahrenheitKeCelcius(string suhuFahrenheit)
+    {
+        double fahrenheit = double.Parse(suhuFahrenheit);
+
+        double celcius = (fahrenheit - 32) * 5 / 9;
+
+        return celcius;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("PROGRAM KONVERSI SUHU");
+        Console.WriteLine("Pilih 1 Untuk konversi dari celcius ke fahrenheit");
+        Console.WriteLine("Pilih 2 Untuk konversi dari celcius ke fahrenheit");
+        Console.Write("Masukkan Pilihan Anda : ");
+
+        string pilihan = Console.ReadLine();
+        double c, f;
+
+        switch (pilihan)
+        {
+            case "1":
+                Console.Write("Masukkan Angka : ");
+                f = KonversiSuhu.celciusKeFahrenheit(Console.ReadLine());
+                Console.WriteLine("Hasilnya Adalah : {0:F2}", f);
+
+
+                break;
+            case "2":
+                Console.Write("Masukkan Angka : ");
+                c = KonversiSuhu.fahrenheitKeCelcius(Console.ReadLine());
+                Console.WriteLine("Hasilnya Adalah : {0:F2}", c);
+
+                break;
+            default:
+                Console.WriteLine("Silahkan Pilih 1 atau 2!");
+
+                break;
+
+        }
+        Console.WriteLine("Tekan enter untuk keluar!");
+        Console.ReadLine();
+
+
+
+
+
+    }
+}
+*/
